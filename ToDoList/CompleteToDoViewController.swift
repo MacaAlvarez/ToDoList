230af8ab9a4dc5db263c9e44f1c9784576f0a160
Macarena Alvarez <alvarezmacuchi@gmail.com>
2022-08-02 14:23:48 -0300

    Initial Commit

---
+++ ToDoList/CompleteToDoViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CompleteToDoViewController.swift
+//  ToDoList
+//
+//  Created by Santiago Alvarez on 02/08/2022.
+//
+
+import UIKit
+
+class CompleteToDoViewController: UIViewController {
+    
+    var previousVC = TwoTableViewController()
+    var selectedToDo = ToDo()
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = selectedToDo.name
+
+        // Do any additional setup after loading the view.
+   
+    }
+    
+    @IBAction func completeTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Santiago Alvarez on 02/08/2022.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
